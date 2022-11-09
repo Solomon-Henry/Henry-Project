1b9ff920956245a95c83f74c54dfa2620ca6c887
Solomon-Henry <cherm005@gmail.com>
2022-11-09 03:47:53 -0500

    Project 3 Completed

---
+++ Location_Data.py
@@ -0,0 +1,113 @@
+from Text_Data import*
+Location_Dict = {
+    "Start" : {
+        "Name" : "",
+        "Text_List" : Stop_A_Text_List,
+        "Examine" : ""
+    },
+    "Quit" : {
+        "Name" : "",
+        "Text_List" : Stop_Quit_Text_List,
+        "Examine" : ""
+    },
+    "Death" : {
+        "Name" : "",
+        "Text_List" : Stop_Death_Text_List,
+        "Examine" : ""
+    },
+    "Final" : {
+        "Name" : "",
+        "Text_List" : Stop_F_Text_List,
+        "Examine" : ""
+    },
+    1 : {
+        "Name" : "Courtroom",
+        "Text_List" : Stop_0_Text_List,
+        "Examine" : "\nYou're in a courtroom, attesting for your crimes",
+        "Was_Visited" : False
+    },
+    2 : {
+        "Name" : "Dropship",
+        "Text_List" : Stop_1_Text_List,
+        "Examine" : "\nYou're now in a flying dropship, somehwere above the floating city of Olympus",
+        "Was_Visited" : False
+    },
+    3 : {
+        "Name" : "Hammond Labs",
+        "Text_List" : Stop_2_Text_List,
+        "Examine" : "\nYou're at Hammond Labs with an angry enemy squad with guns",
+        "Was_Visited" : False
+    },
+    4 : {
+        "Name" : "Estates",
+        "Text_List" : Stop_3_Text_List,
+        "Examine" : "\nA place with fancy houses for fancy people",
+        "Was_Visited" : False
+    },
+    5 : {
+        "Name" : "Secret Trapdoor",
+        "Text_List" : Stop_4_Text_List,
+        "Examine" : "\nYou're in a Secret Trapdoor, maybe you should get out somehow before danger finds its way to you again",
+        "Was_Visited" : False
+    },
+    6 : {
+        "Name" : "Phase Driver",
+        "Text_List" : Stop_5_Text_List,
+        "Examine" : "\nNothing special here, except the two teams killing each other, nothing out of the ordinary in the Apex games",
+        "Was_Visited" : False
+    },
+    7 : {
+        "Name" : "Solar Array",
+        "Text_List" : Stop_6_Text_List,
+        "Examine" : "\nAs the name suggests, an area solely devoted to having solar panels in arrays",
+        "Was_Visited" : False
+    },
+    8 : {
+        "Name" : "Bonsai Palace",
+        "Text_List" : Stop_7_Text_List,
+        "Examine" : "\nA palace with so many beautiful artworks. They surely wouldn't miss a few dozen of them, right?",
+        "Was_Visited" : False
+    },
+    9 : {
+        "Name" : "Champion's Podium",
+        "Text_List" : Stop_8_Text_List,
+        "Examine" : "\nThis is where champions are found, like you",
+        "Was_Visited" : False
+    },
+    10 : {
+        "Name" : "Prison Dropship",
+        "Text_List" : Stop_9_Text_List,
+        "Examine" : "\nYou're now going back to jail in a Prison Dropship, you didn't forget you were a criminal right?",
+        "Was_Visited" : False
+    },
+    11 : {
+        "Name" : "Oasis",
+        "Examine" : "A nice tourist trap with lots of places to relax, conveniently placed right at the edge of the map"
+    },
+    12 : {
+        "Name" : "Turbine",
+        "Examine" : "\nA place wih a giant turbine which keeps the floating city of Olympus afloat"
+    },
+    13 : {
+        "Name" : "Energy Deposit",
+        "Examine" : "\nJust Olympus's main power grid. May be important later"
+    },
+    14 : {
+        "Name" : "Grow Towers",
+        "Examine" : "\nA place where large spiraling towers are covered with greenery"
+    },
+    15 : {
+        "Name" : "Hydroponics",
+        "Examine" : "\nA small part of Olympus that is host to many plants, serving as their watering system"
+    },
+    16 : {
+        "Name" : "Orbital Cannon",
+        "Examine" : "\nOrbital Cannon. It's name speaks for itself"
+    },
+    17 : {
+        "Name" : "Icarus",
+        "Examine" : "\nA wrecked ship containing the deadlist virus in the the galaxy. Best not to go there"
+    }
+}
+
+
--- adventure.py
+++ adventure.py
@@ -1,210 +1,189 @@
 # New Reworked Version for Project 3. (Unfinished Sketch)
-# To Be Done
-# Bring the missing variables from the old_adventure file and reformat them when necessary
-# Use previously stated variables for the correct initialization of the Stop instances
-# Properly initialize Location_List, Map, and any variables needed for the proper and 
-#Complete the functions Divider, Command_Index, and implement them in the Story Sequence method.
-# Complete the Stop class' methods: Story_Sequence and Setter. 
-# Fix the function Display_Progress 
 
-## Variables 
-# (Missing many necessary variables, so the stop class instances are filled with dummy data)
-# (Bring the needed variables from the old_adventure file and reformat them if necessary)
-
-
+from Location_Data import Location_Dict
 
 Quit_Status:bool = False
+Death_Status:bool = False
+Progress:int = 0
 Name:str = ""
+Score:int = 0
 
 
-# Text related variables
-# Key: (TEXT: regular text) (PROMPT: prompt for user to continue or make a choice) (PATH: To create alt path)
-# Make a list for alt_lists
+def min_max(min,max,val):
+    ans = val
+    if val < min:
+        ans = min
+    elif val > max:
+        ans = max
+    return ans
+def Get_map_coord(val):
+    coords = [None,None]
+    str = Location_Dict[val]["Name"]
+    for i in range(len(Map)):
+        for j in range(len(Map[i])):
+            if Map[i][j] == str:
+                coords[0] = i
+                coords[1] = j
+                break
+    return coords
 
-
-
-
-# Location_List (List of all locations with each index referring to the name of a location as a string)
-Location_List = [
-    None,
-    "Courtroom",
-    "Dropship",
-    "Hammond Labs",
-    "Estates",
-    "Secret Trapdoor",
-    "Phase Driver",
-    "Solar Array", 
-    "Bonsai Palace",
-    "Champion's Palace",
-    "Prison Dropship",
-    None,
-]
-# Location_List_Info (A dictionary of all the locations along with their corresponding in the game) 
-# (Value stored as stop num and info)
-# (The original travelable locations are numbered from 1-9, all other locations will be numbered outside of range) 
-# To be completed and filled with meaningful values
-Location_List_Info = {
-    "Courtroom" : [[0,2],"Courts bois"]
-
-}
-# Map (A matrix where all the in-game locations are placed as they would be, representing travel destinations)
-# To be completed and filled with meaningful values
 Map = [
    ["OOB","Oasis","Turbine","Energy Deposit"],
    ["OOB","Estates","Hammond Labs","Grow Towers"],
    ["Hydroponis","Phase Driver","Solar Array","Orbital Cannon"],
-   ["OOB","OOB","Bonsai Palaca","Icarus"],
+   ["OOB","OOB","Bonsai Palace","Bonsai Palace","Icarus"],
    ["OOB","OOB","OOB","OOB"]
 ]
+User_Commands = ["EXAMINE","HELP","QUIT"]
+Text_Commands = ["TEXT","PROMPT","SET","CHOICE"]
 
-# User Commands(All viable commands which user can use whennever they aren't at a fixed or final continue, with some exceptions)
-# The first 3 commands can be paired with a cardinal direction or 'CL' to either move the user there or look/examine the area
-# Typing in Look or Examine will print out corresponding info for current and all nearby locations
-User_Commands = ["GO","LOOK","EXAMINE","HELP","QUIT"]
-# Text Commands (For Story_Sequence method to know if to make string a regular text, prompt, variable, make new path, return to old path, or dead end )
-Text_Commands = ["TEXT","PROMPT","SET","PATH","RETURN","DEATH"]
-## Stops
-# Note (User_Script is now Fixed_Cont)
-# Alternate paths are only in mini_stops
-# Final Continues are transitions between stops
-# Methods: Story_Sequence (Maybe automatically uses asterisk separator)(Can use alternate paths)(Can handle commands especially QUIT)
 class Stop:
-    def __init__(Stop,LocationID,TextList):
-        Stop.Location = Location_List[LocationID]
-        Stop.Location_Info = Location_List_Info[Stop.Location][1]
-        Stop.Map_Coordinates = Location_List_Info[Stop.Location][0]
-        Stop.Text_List = TextList
-        Stop.Skips = 0
-        Stop.Final_Continue = TextList[-1]
-    def Text_Divider(Stop):
-        pass
+    def __init__(Stop,LocationID):
+        
+        Stop.Id = LocationID
+        Stop.Location = Location_Dict[Stop.Id]
+        Stop.Name = Stop.Location["Name"]
+        Stop.Location_Info = Stop.Location["Examine"]
+        Stop.Map_Coordinates = Get_map_coord(Stop.Id)
+        Stop.Text_List = Stop.Location["Text_List"]
     def Text_Command(Stop,str):
         #Finds text command in between two of the following character |
+        global Name,Death_Status
+        Text = str
         command = str[str.index("|")+1:str[str.index("|")+1:].index("|")+str.index("|")+1]
+        Name_Replace_Index = str.find("|USERNAME|")
+        if Name_Replace_Index != -1:
+            Text = Text[:Name_Replace_Index] + Name + Text[Name_Replace_Index+len("|USERNAME|"):]
         if command == Text_Commands[0]:
-            #Text; Functional
-            print_str = str[len(command)+2:]
+            #Text
+            print_str = Text[len(command)+2:]
             print(print_str)
-            pass
         elif command == Text_Commands[1]:
-            #Prompt: Not Finished, set up user_command function
-            prompt_str = str[len(command)+2:]
+            #Prompt
+            prompt_str = Text[len(command)+2:]
             user_response = input(prompt_str)
             Stop.User_Command(user_response)
-            Stop
-            pass
-        elif command[:-1] == Text_Commands[2]:
-            #Set: Not Finished
-            set_str = str[len(command)+2:]
-            set = input(set_str)
-            Name = set
-            # Change corresponding variables that need users name
-            pass
-        elif command[:-1] == Text_Commands[3]:
-            #Path, Will call iteration to iterate through alt path and update Stop.Skips so that when returning to
-            # original path, the skipped mini stops will actually be skipped
-            # Not Finished
-            pass
-        elif command[:-1] == Text_Commands[4]:
-            #Return, bring user back to original path
-            # Not Finished
-            pass
-        elif command == Text_Commands[5]:
-            #Death, Not Finished
-            pass
-        pass
+        elif command == Text_Commands[2]:
+            #Set
+            set_str = Text[len(command)+2:]
+            Name = input(set_str)
+        elif command == Text_Commands[3]:
+            Correct_Continue = Text[len(command)+2:]
+            Correct_Answer = Location_Dict[int(Stop.Id)+1]["Name"] 
+            N = Map[min_max(0,4,Stop.Map_Coordinates[0]-1)][Stop.Map_Coordinates[1]]
+            S = Map[min_max(0,4,Stop.Map_Coordinates[0]+1)][Stop.Map_Coordinates[1]]
+            W = Map[Stop.Map_Coordinates[0]][min_max(0,3,Stop.Map_Coordinates[1]-1)]
+            E = Map[Stop.Map_Coordinates[0]][min_max(0,3,Stop.Map_Coordinates[1]+1)]
+            Directions = [N,S,W,E]
+            Options = []
+            Location_Dict_Indexes = []
+            Printed_Options = []
+            for c in Directions:
+                if not (c == "OOB" or c == Map[Stop.Map_Coordinates[0]][Stop.Map_Coordinates[1]]):
+                    Options.append(c)
+            for j in Options:
+                for i in Location_Dict:
+                    if Location_Dict[i]["Name"] == j:
+                        Location_Dict_Indexes.append(i)
+            for k in Location_Dict_Indexes:
+                Printed_Options.append([Location_Dict[k]["Name"],Location_Dict[k]["Examine"]])
+            options_str = "\n"
+            for m in Printed_Options:
+                options_str = "\n" + m[0] + ": " + m[1]
+                print(options_str) 
+            user_response = input("\nChoose one of the options above, i.e the corresponding name: ") 
+            if user_response == Correct_Answer:
+                print("\nGreat choice. You continue with your life.")
+                input(Correct_Continue)
+            else: 
+                input("\nBad decision, you will now be dead\nPress enter to continue")
+                Death_Status =  True     
     def User_Command(Stop,user_str):
+        global Quit_Status
         for i in User_Commands:
             #Functionality for user_command to be set up
             if user_str.find(i) != -1:
-                if i == "GO":
-                    pass
-                elif i == "LOOK":
-                    pass
-                elif i == "EXAMINE":
+                if i == "EXAMINE":
+                    print(Stop.Location_Info)
+                    input("\nPress Enter to Continue")
                     pass
                 elif i == "HELP":
-                    pass
+                    print("\nValid Commands:\nEXAMINE: Find info on current location\nHELP: Display all valid commands\nQUIT: End game abruptly")
+                    input("\Press enter to continue")
                 elif i == "QUIT":
+                    input("\nYou have quit the game.\nPress enter to continue to the end of the game :(")
+                    Quit_Status = True
                     pass
                 pass
         pass
     def Display_Progress(Stop):
-        pass
-    def Iterate(arr):
-        for i in range(len(arr)):
-            Stop.Text_Command(i)
-            Stop.Skips = Stop.Skips + 1
-            pass
-        pass
+        global Score,Progress
+        Story_Progress:int
+        if str(Stop.Id).isnumeric():
+            Progress = Progress + 1
+            Story_Progress = (Progress/10) * 100
+            print(f"\nStory Progress: {Story_Progress}%\nTotal Score: {Score} points")
     def Story_Sequence(Stop): # To be completed
-        # Iterate through array
-        # Act based on text command keyword in string
-        # Be able to use text, prompts and setters
-        # How to get deaths to work
+        global Score
+        if (str(Stop.Id).isnumeric()) and (int(Stop.Id) >= 1 and int(Stop.Id)<=10) and (Stop.Location["Was_Visited"] == False):
+            Score = Score + 10
+            Stop.Location["Was_Visited"] == True
         for i in Stop.Text_List:
-            if Stop.Skips != 0:
-                Stop.Skips = Stop.Skips - 1
-                continue
+            if Quit_Status or Death_Status:
+                break
+            print("****************************************")
             Stop.Text_Command(i)
+            print("****************************************")
             if Stop.Text_List.index(i) == len(Stop.Text_List)-1:
                 Stop.Display_Progress()
-        pass
-c = ["|TEXT|\naaaa"]
-Stop_Test:Stop = Stop(1,c)
-Stop_Test.Story_Sequence()
-# Individual instances of Stops to be defined properly, shown values are simply dummy ones for the sole purpose of testing
 
-Stop_1:Stop
-Stop_2:Stop
-Stop_3:Stop
-Stop_4:Stop 
-Stop_5:Stop
-Stop_6:Stop 
-Stop_7:Stop 
-Stop_8:Stop 
-Stop_9:Stop
-Stop_10:Stop 
-Stop_Start:Stop 
-Stop_Quit:Stop 
-Stop_Final:Stop 
-#Stop_List = [Stop_Start,Stop_1,Stop_2,Stop_3,Stop_4,Stop_5,Stop_6,Stop_7,Stop_8,Stop_9,Stop_10,Stop_Quit,Stop_Final]
-Stop_List = []
-
-
+Stop_1:Stop = Stop(1)
+Stop_2:Stop = Stop(2)
+Stop_3:Stop = Stop(3)
+Stop_4:Stop = Stop(4)
+Stop_5:Stop = Stop(5)
+Stop_6:Stop = Stop(6)
+Stop_7:Stop = Stop(7)
+Stop_8:Stop = Stop(8)
+Stop_9:Stop = Stop(9)
+Stop_10:Stop = Stop(10)
+Stop_Start:Stop = Stop("Start")
+Stop_Quit:Stop = Stop("Quit")
+Stop_Death:Stop = Stop("Death")
+Stop_Final:Stop = Stop("Final")
+Stop_List = [Stop_Start,Stop_1,Stop_2,Stop_3,Stop_4,Stop_5,Stop_6,Stop_7,Stop_8,Stop_9,Stop_10,Stop_Quit,Stop_Death,Stop_Final]
 
 ## Game_Loop (Done)
-# iterate through the Stops classes (Numericals only) (Change Stop A to numerical)
-# break if Quit_Status is true
-# use Story_Sequence method for each Stop class in Stop_list
-# Display Progress 
-# Print Final_Continue attribute as an input
 def Game_Loop():
     for Places in Stop_List:
-        if (Stop_List.index(Places) >= len(Stop_List)-2):
+        if (Stop_List.index(Places) >= len(Stop_List)-3):
             break
         elif Quit_Status:
             Stop_Quit.Story_Sequence()
             break
+        elif Death_Status:
+            Stop_Death.Story_Sequence()
+            break
         Places.Story_Sequence()
     Restart_Prompt()
 
-## Restart_Prompt (Done)
-# prompt user to restart
-# if yes then call another Game_Loop function
-# else use Story_Sequence method for Stop_Final
 def Restart_Prompt():
-    ans = input("You can choose to restart by typing 'yes' or quit by pressing enter: ")
+    global Quit_Status,Death_Status,Progress,Name,Score
+    if (not Quit_Status) and (not Death_Status) :
+        ans = input("\nGreat job on finishing the game!\nYou can restart by typing 'yes' or quit by pressing enter here: ")
+    else:
+        ans = input("You can choose to restart by typing 'yes' or quit by pressing enter: ")
     if ans == "yes":
+        Quit_Status = False
+        Death_Status = False
+        Progress = 0
+        Name = ""
+        Score = 0
         Game_Loop()
     else:
         Stop_Final.Story_Sequence()
 
-
-## Main (Done)
-# Calls and initiates the Game_loop
-# 
 def main():
     Game_Loop()
     
-#main()
+main()
